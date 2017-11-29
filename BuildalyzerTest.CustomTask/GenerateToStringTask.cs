eaedd1fee60748a639d89a5416a48a74a3997ffa
itn3000 <itn3000@gmail.com>
2017-11-30 03:48:10 +0900

    wip commit

--- BuildalyzerTest.CustomTask/GenerateToStringTask.cs
+++ BuildalyzerTest.CustomTask/GenerateToStringTask.cs
@@ -38,12 +38,42 @@ namespace BuildalyzerTest.CustomTask
         public bool Initialize(string taskName, IDictionary<string, MSBFramework.TaskPropertyInfo> parameterGroup, string taskBody, MSBFramework.IBuildEngine taskFactoryLoggingHost)
         {
             m_Parameters = parameterGroup.Values.ToArray();
+            try
+            {
+                if (System.AppDomain.CurrentDomain != null)
+                {
+                    System.AppDomain.CurrentDomain.AssemblyResolve += (sender, e) =>
+                    {
+                        taskFactoryLoggingHost.LogMessageEvent(new MSBFramework.BuildMessageEventArgs($"resolving {e.Name}", "AssemblyResolve", "AssemblyResolve", MSBFramework.MessageImportance.Normal));
+                        return Assembly.Load(new AssemblyName(e.Name));
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
             return true;
         }
     }
 
     public class GenerateToStringTask : MSBUtil.Task
     {
+        void ProcessWorkspace(ProjectAnalyzer projectAnalyzer)
+        {
+            Log.LogMessage("get workspace");
+            var ws = projectAnalyzer.GetWorkspace();
+            foreach (var proj in ws.CurrentSolution.Projects)
+            {
+                if (proj.TryGetCompilation(out var compilation))
+                {
+                    foreach (var typeSymbol in compilation.GetSymbolsWithName(_ => true, SymbolFilter.Type).OfType<ITypeSymbol>())
+                    {
+                        Log.LogMessage("typename={0}", typeSymbol.Name);
+                    }
+                }
+            }
+        }
         void DoTask()
         {
             var projectFile = this.BuildEngine.ProjectFileOfTaskNode;
@@ -51,17 +81,16 @@ namespace BuildalyzerTest.CustomTask
             var analyzeManager = new AnalyzerManager();
             Log.LogMessage("get project info:{0}", projectFile);
             var projectAnalyzer = analyzeManager.GetProject(projectFile);
-            // Log.LogMessage("get workspace");
-            // var ws = projectAnalyzer.GetWorkspace();
-            // foreach (var proj in ws.CurrentSolution.Projects)
+            ProcessWorkspace(projectAnalyzer);
+            // var projectInstance = projectAnalyzer.Compile();
+            // if(projectInstance != null)
             // {
-            //     if (proj.TryGetCompilation(out var compilation))
+            //     foreach(var item in projectInstance.GetItems("Compile"))
             //     {
-            //         foreach (var typeSymbol in compilation.GetSymbolsWithName(_ => true, SymbolFilter.Type).OfType<ITypeSymbol>())
-            //         {
-            //             Log.LogMessage("typename={0}", typeSymbol.Name);
-            //         }
+            //         Log.LogMessage($"{item.ItemType},{item.EvaluatedInclude}");
             //     }
+            // }else{
+            //     Log.LogMessage("projectInstance is NULL");
             // }
         }
         public override bool Execute()
