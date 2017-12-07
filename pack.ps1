f65d29d2bc4308d7eaa84918657945714a7cbbb3
itn3000 <itn3000@gmail.com>
2017-12-08 06:22:48 +0900

    remove msbuild implementations

--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,3 @@
-Push-Location BuildalyzerTest.CustomTask
-dotnet pack -c Release -o ../nupkg
-Pop-Location
-
 Push-Location -Path .\dotnet-buildalyzer-test
 dotnet pack -c Release -o ../nupkg
 Pop-Location
